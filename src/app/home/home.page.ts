db8bf7b48d09a5c3122a968001b30f6859fb4a62
Nazim Hickmet <toto@mail.com>
2019-03-26 12:01:25 +0100

    typescript premiers pas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private firstName: string;
+  private age:number;
 
+  private buttonColor: string = "secondary";
+
+  private fruitList: Array<string> = [
+    "Pommes", "Poires", "Oranges" 
+  ];
+
+  private person:Object = {
+    name: "Alphonse",
+    address: "5 rue Orfila",
+    country: "France"
+  };
+
+  private amountInEuro:number = 1;
+
+  private changeRate: number = 124.95;
+
+  constructor(){
+    this.firstName = "Siobbhan";
+  }
+
+  changeName(){
+    this.firstName = "Aileen";
+    this.buttonColor = this.buttonColor=="danger"?"secondary":"danger";
+  }
 }
